b97278be9b473e3bc0a8fa5f9e1f6379ff576e2c
fr3dy5 <fr3dy5al@gmail.com>
2024-10-19 18:02:28 -0500

    added dice and reroll variables, iteration loop, and output writelines

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿dice[i];
+rerolls;
+
+
+for (int i = 1; i < 6; i++) {
+    for (dice[i];  )
+}
+
+
+
+Console.WriteLine("Initial Roll:");
+Console.WriteLine($"Die 1: {dice[1]}");
+Console.WriteLine($"Die 1: {dice[2]}");
+Console.WriteLine($"Die 1: {dice[3]}");
+Console.WriteLine($"Die 1: {dice[4]}");
+Console.WriteLine($"Die 1: {dice[5]}");
+
+Console.WriteLine("Enter the dice numbers to reroll (comma-separated, ex: 2,3,5) or leave blank to keep current dice.");
+Console.Write($"You have {rerolls} rerolls remaining): ");
+Console.ReadLine();
